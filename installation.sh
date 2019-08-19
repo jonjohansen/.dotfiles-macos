dc67f3f5618c7c51d9c5ad1805b2a92519a945a8
Jon Johansen <jonjohansen2k@gmail.com>
2019-08-19 23:07:25 +0200

    🌪 Add git configs and ignores

--- installation.sh
+++ installation.sh
@@ -1,9 +1,10 @@
 ### Should start the installation ###
+export DOTFILES="$HOME/.dotfiles-macos"
 
-# Get sudo from user for this session
+# Get sudo from user for this session, just in case
 sudo -v 
 
-# Install brew
+ Install brew if it is not installed
 if test ! $(which brew); then
   ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 fi
@@ -13,5 +14,20 @@ brew upgrade
 brew tap homebrew/bundle
 brew bundle
 
-# Handle iterm2 config after installing it
+# Set iterm settings to target config file
 ./iterm2.sh 
+
+# Install the settings for the mac
+./macos.sh
+
+# Remove zshrc if it exists, and symlink this in
+rm -rf $HOME/.zshrc
+ln -s $DOTFILES/.zshrc $HOME/.zshrc
+
+## Global git config
+# Replace .gitconfig with symlink here
+rm -rf $HOME/.gitconfig
+ln -s $DOTFILES/git/.gitconfig $HOME/.gitconfig
+# Replace .gitignore with symlink here
+rm -rf $HOME/.gitignore
+ln -s $DOTFILES/git/.gitignore $HOME/.gitignore
