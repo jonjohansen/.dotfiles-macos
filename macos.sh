c8a1893aa47b21b890d1db8accf8649db666721d
Jon Johansen <jonjohansen2k@gmail.com>
2019-08-20 23:21:32 +0200

    👹 Forgot to add flag for executeable

--- macos.sh
+++ macos.sh
@@ -1,2 +1,7 @@
+### MacOS settings ###
+ 
 # Set scrolling direction
 defaults write NSGlobalDomain com.apple.swipescrolldirection -bool false
+
+# Prevent Photos from opening automatically when devices are plugged in
+defaults -currentHost write com.apple.ImageCapture disableHotPlug -bool true
